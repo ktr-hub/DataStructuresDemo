c7d9da4e5889d42387929df37c642826c22af38c
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-21 19:55:54 +0530

    [Tirupathi] Add. Ability to create Ordered Linked List

---
+++ DataStructuresDemo/OrderedLinkedList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresDemo
+{
+    class OrderedLinkedList
+    {
+        internal Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+
+                if (head.data.CompareTo(data) >= 0)
+                {
+                    node.next = head;
+                    head = node;
+                }
+                else
+                {
+                    Node tempPrev = head;
+                    while (temp != null)
+                    {
+                        tempPrev = temp;
+                        if (temp.data.CompareTo(data) >= 0)
+                        {
+                            node.next = temp;
+                            tempPrev.next = node;
+                            break;
+                        }
+                        temp = temp.next;
+                    }
+                    if (temp == null)
+                    {
+                        tempPrev.next = node;
+                    }
+                }
+            }
+            Console.WriteLine("Data added "+node.data+" successfully");
+        }
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("\nNo data");
+            }
+            else
+            {
+                Console.WriteLine("\nOrdered Linked List Stack : ");
+                while (temp.next != null)
+                {
+                    Console.Write(temp.data+"->");
+                    temp = temp.next;
+                }
+                Console.Write(temp.data);
+            }
+        }
+    }
+}
--- DataStructuresDemo/Program.cs
+++ DataStructuresDemo/Program.cs
@@ -7,27 +7,12 @@ namespace DataStructuresDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Linked List Demo!");
-            LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(90);
-            list.Add(100);
-            int index = list.GetIndex(56);
-            list.InsertAtParticularPositon(index+1, 99);
-            list.Display();
-            Console.WriteLine("\nCount of items is : " + list.CountItems());
-            list.Pop();
-            list.Display();
-            Console.WriteLine("\nCount of items is : " + list.CountItems());
-
-            list.PopLast();
-            list.Display();
-            Console.WriteLine("\nCount of items is : " + list.CountItems());
-
-            list.Remove(90);
-            list.Display();
-            Console.WriteLine("\nCount of items is : " + list.CountItems());
-
+            OrderedLinkedList orderedLinkedList = new OrderedLinkedList();
+            orderedLinkedList.Add(56);
+            orderedLinkedList.Add(40);
+            orderedLinkedList.Add(70);
+            orderedLinkedList.Add(10);
+            orderedLinkedList.Display();
         }
     }
 }
